0abad5aa2e585414e1beee35c791780d5154915c
Yang Liang <liang3.yang@intel.com>
2024-06-18 16:22:30 +0800

    add allgather all2all reduce_scatter support

--- term.sh
+++ term.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -x 
 
 #delete previous jobs
-kubectl delete -f job/1.15.1/all_reduce/all/hccl_demo_ar_16cards.yaml >/dev/null 2>&1
-#kubectl delete mpijob alibaba-llamav2-70b-32 -n habanalabs   >/dev/null 2>&1
+kubectl delete -f job/1.15.1/${2}/all/hccl_demo_ar_${1}.yaml >/dev/null 2>&1
 
 terminating_llama_pod=$(kubectl get po -n alibaba | grep  alibaba | grep Terminating)
 if [ -n "$terminating_llama_pod" ]; then
